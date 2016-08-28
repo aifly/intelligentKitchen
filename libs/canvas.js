b67b1923d2ba9d1c1972a3b32919f9b3dbf8e068
aifly <aifly_xc@163.com>
2016-08-28 19:42:49 +0800

    draw  time

--- libs/canvas.js
+++ libs/canvas.js
@@ -15,19 +15,41 @@ export default class Time{
 		let s = this;
 		var context = s.canvas.getContext('2d');
 		var ten = s.hours / 10 | 0,
-			one = s.hours % 10;
+			one = s.hours % 10,
+			minsTen = s.mins/10|0,
+			minsOne = s.mins%10;
 
 
-		for(var i = 0,len = 20;i<len;i++){
-			for(var j = 0,len1 = 34;j<len1;j++){
+		for(var i = 0;i<9;i++){
+			for(var j = 0;j<34;j++){
 				context.beginPath();
 				context.arc((s.margin+s.r)*j+s.margin,i*(s.margin+s.r)+s.margin,s.r,0,Math.PI*2,false);
 				context.closePath();
 				
-				context.fillStyle='#e89e79';
-				if(numberData[ten][i][j]){
-					context.fillStyle='red';	
+				context.fillStyle='#f8f3ef';
+				if(numberData[ten][i] && numberData[ten][i][j]){
+					context.fillStyle='#df9977';	
 				}
+
+				context.fill();
+			}
+		}
+		id = numberData[ten][0].length;
+
+
+		for(var i = 0;i<9;i++){
+			for(var j = 0;j<34;j++){
+				context.beginPath();
+
+				context.arc((s.margin + s.r)*(j+id)+s.margin,i*(s.margin+s.r)+s.margin,s.r,0,Math.PI*2,false);
+				
+				context.closePath();
+
+				context.fillStyle='#f8f3ef';
+				if(numberData[one][i] && numberData[one][i][j]){
+					context.fillStyle='#df9977';	
+				}
+				
 				context.fill();
 			}
 		}
