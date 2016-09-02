a0a5c0335d968d2d1f036b11d50b4754c5b54ac9
aifly <aifly_xc@163.com>
2016-09-02 18:06:55 +0800

    a

---
+++ libs/sector.js
@@ -0,0 +1,39 @@
+
+ class Sector{
+	constructor(props={}) {
+		let s = this;
+		s.x = props.x;
+		s.y = props.y;
+		s.r = props.r;
+		s.startAngle = (props.startAngle)/180*Math.PI;
+		s.endAngle = (props.startAngle+45)/180*Math.PI;
+		s.color = props.color||'#fff';
+		s.rotate = props.rotate;
+		s.stage = props.stage;
+		s.draw();
+	}
+
+	draw(){
+		let shape = new createjs.Shape(),
+			s = this;
+		shape.graphics.beginFill(s.color)
+			.moveTo(s.x,s.y)
+			.lineTo(s.x+s.r*Math.cos(s.startAngle),s.y+s.y*Math.sin(s.startAngle))
+			.arc(s.x,s.y,s.r,s.startAngle,s.endAngle,0)
+			.lineTo(s.x,s.y)
+			.endFill().endStroke();
+		s.stage.addChild(shape);
+		shape.rotation = s.rotate;
+		shape.x = s.x;
+		shape.y = s.y;
+		shape.regX = s.x;
+		shape.regY = s.y;
+		
+		
+
+	}
+
+}
+
+
+export default {Sector};
