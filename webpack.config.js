f1da9ecb96a203e36b976d15f351eca0e43a275d
aifly <aifly_xc@163.com>
2017-09-15 18:59:06 +0800

    modify the port

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,30 @@
 var webpack = require('webpack');
 
 module.exports = {
-	entry:{
-		index:'./index.jsx'
+	entry: {
+		index: './index.jsx'
 	},
-	output:{
-		path:'./assets/js',
-		filename:'[name].js'
+	output: {
+		path: './assets/js',
+		filename: '[name].js'
 	},
-	devServer:{
-		inline:true,
-		hot:true,
-		port:4000
+	devServer: {
+		inline: true,
+		hot: true,
+		port: 3999
 	},
-	module:{
-		loaders:[
-			{
-				test:/\.jsx|\.js$/,
-				exclude:/node_modules/,
-				loaders:['babel']
-			},
-			{
-				test:/\.css$/,
-				exclude:/node_modules/,
-				loader:'style-loader!css-loader'
-			},
-			{
-				test:/\.png|\.jpg|\.gif$/,
-				loaders:['url-loader?limit=30720']
-			}
-		]
+	module: {
+		loaders: [{
+			test: /\.jsx|\.js$/,
+			exclude: /node_modules/,
+			loaders: ['babel']
+		}, {
+			test: /\.css$/,
+			exclude: /node_modules/,
+			loader: 'style-loader!css-loader'
+		}, {
+			test: /\.png|\.jpg|\.gif$/,
+			loaders: ['url-loader?limit=30720']
+		}]
 	}
 };
