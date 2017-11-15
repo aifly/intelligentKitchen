b9bd511a6f8b1b13823b3f04c462be20b181bc4d
aifly <aifly_xc@163.com>
2017-11-15 10:16:55 +0800

    a

--- libs/url.js
+++ libs/url.js
@@ -1,14 +1,12 @@
-
-
 const URL = {
-	baseURL:window.baseURL,
-	weightstart:window.baseURL + 'kb/kitchen/weighstart.ht',//去皮接口
-	weightend:window.baseURL + 'kb/kitchen/weighend.ht',//确定称重。
-	getCookBookList:window.baseURL + 'kb/kitchen/getcookbook.ht',//根据类型获取菜谱列表
-	getBookList:window.baseURL + 'kb/kitchen/getbooklist.ht',//求取未来14天的菜谱情况
-	getimespan:window.baseURL + 'kb/kitchen/getimespan.ht',//上传时间
-	setScale:window.baseURL + 'kb/kitchen/setscale.ht',//显示全部营养成分
-	getKeepCookbook:window.baseURL + 'kb/kitchen/getKeepCookbook.ht',//冬季推荐菜谱
+	baseURL: window.baseURL,
+	weightstart: window.baseURL + '/kitchen/weighstart.ht', //去皮接口
+	weightend: window.baseURL + '/kitchen/weighend.ht', //确定称重。
+	getCookBookList: window.baseURL + '/kitchen/getcookbook.ht', //根据类型获取菜谱列表
+	getBookList: window.baseURL + '/kitchen/getbooklist.ht', //求取未来14天的菜谱情况
+	getimespan: window.baseURL + '/kitchen/getimespan.ht', //上传时间
+	setScale: window.baseURL + '/kitchen/setscale.ht', //显示全部营养成分
+	getKeepCookbook: window.baseURL + '/kitchen/getKeepCookbook.ht', //冬季推荐菜谱
 }
 
 export default URL;
